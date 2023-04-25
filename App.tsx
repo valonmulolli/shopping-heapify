304d357c7eb96254d26cddc1a0eef2f7e7945e04
valon <valonmulolli@gmail.com>
2023-04-25 15:35:10 +0200

    header

--- App.tsx
+++ App.tsx
@@ -1,23 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import RootNavigator from './src/navigators/RootNavigator';
-import { NavigationContainer } from '@react-navigation/native';
-
-
+import {
+	DefaultTheme,
+	NavigationContainer,
+	Theme,
+	useTheme,
+} from '@react-navigation/native';
+import { useMemo } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-      <StatusBar style="dark" />
-    </View>
-  );
+	const theme: Theme = useMemo(
+		() => ({
+			...DefaultTheme,
+			colors: { 
+        ...DefaultTheme.colors, 
+        background: '#f5f5f5',
+        text: '#191919',
+        border: '#D9D9D9',
+        primary: '#191919',
+      },
+		}),
+		[]
+	);
+	return (
+		<View style={styles.container}>
+			<NavigationContainer theme={theme}>
+				<RootNavigator />
+			</NavigationContainer>
+			<StatusBar style='dark' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+	container: {
+		flex: 1,
+	},
 });
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,65 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, Image, ScrollView, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useTheme } from '@react-navigation/native';
+import Icons from '@expo/vector-icons/MaterialIcons';
+
+const AVATAR_URL =
+	'https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80';
 
 const HomeScreen = () => {
+	const { colors } = useTheme();
+
 	return (
 		<ScrollView>
 			<SafeAreaView>
-				<Text>HomeScreen</Text>
+				<View
+					style={{
+						paddingHorizontal: 24,
+						paddingVertical: 24,
+						flexDirection: 'row',
+						alignItems: 'center',
+						gap: 8,
+					}}
+				>
+					<Image
+						source={{ uri: AVATAR_URL }}
+						style={{ width: 52, aspectRatio: 1, borderRadius: 52 }}
+						resizeMode='cover'
+					/>
+					<View style={{ flex: 1 }}>
+						<Text
+							style={{
+								fontSize: 20,
+								fontWeight: '600',
+								marginBottom: 8,
+								color: colors.text,
+							}}
+							numberOfLines={1}
+						>
+							Hi Ana! 👋
+						</Text>
+						<Text
+							style={{ color: colors.text, opacity: 0.5 }}
+							numberOfLines={1}
+						>
+							Discover Fashion that suit your style
+						</Text>
+					</View>
+					<TouchableOpacity
+						style={{
+							width: 52,
+							aspectRatio: 1,
+							alignItems: 'center',
+							justifyContent: 'center',
+							borderRadius: 52,
+							borderWidth: 1,
+							borderColor: colors.border,
+						}}
+					>
+						<Icons name='notifications' size={24} color={colors.text} />
+					</TouchableOpacity>
+				</View>
 			</SafeAreaView>
 		</ScrollView>
 	);
